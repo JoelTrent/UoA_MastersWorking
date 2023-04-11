a7e87aca463d09aa0bc688720857c7771d985453
JoelTrent <79883375+JoelTrent@users.noreply.github.com>
2023-04-11 15:03:18 +1200

    Considered how to use gradient information in continuation method, and whether gradient computed is the correct size and direction

---
+++ 2DGradientLinesearchTests.jl
@@ -0,0 +1,219 @@
+using LinearAlgebra: norm, dot
+
+# Section 1: set up packages and parameter values
+using Plots, DifferentialEquations
+using .Threads 
+using Interpolations, Random, Distributions
+using BenchmarkTools
+gr()
+
+Random.seed!(12348)
+fileDirectory = joinpath("Workflow paper examples", "Logistic Model Num Opt", "Plots")
+# include(joinpath("..", "plottingFunctions.jl"))
+include("JuLikelihood.jl")
+
+# Workflow functions ##########################################################################
+
+# Section 2: Define ODE model
+function DE!(dC, C, p, t)
+    λ,K=p
+    dC[1]= λ * C[1] * (1.0 - C[1]/K)
+end
+
+# Section 3: Solve ODE model
+function odesolver(t, λ, K, C0)
+    p=(λ,K)
+    tspan=(0.0, t[end])
+    prob=ODEProblem{true, SciMLBase.FullSpecialize}(DE!, [C0], tspan, p)
+    sol=solve(prob, saveat=t)
+    return sol[1,:]
+end
+
+# Section 4: Define function to solve ODE model 
+function ODEmodel(t, a)
+    y=odesolver(t, a[1],a[2],a[3])
+    return y
+end
+
+# Section 6: Define loglikelihood function
+function loglhood(a, data)
+    y=ODEmodel(data.t, a)
+    return sum(loglikelihood(data.dist, data.yobs-y))
+end
+
+
+# Section 8: Function to be optimised for MLE
+# note this function pulls in the globals, data and σ and would break if used outside of 
+# this file's scope
+function funmle(a); return loglhood(a, data) end 
+
+# Data setup #################################################################################
+# true parameters
+λ=0.01; K=100.0; C0=10.0; t=0:100:1000; σ=10.0;
+tt=0:5:1000
+a=[λ, K, C0]
+
+# true data
+ytrue = ODEmodel(t, a)
+
+# noisy data
+yobs = ytrue + σ*randn(length(t))
+
+# Named tuple of all data required within the likelihood function
+data = (yobs=yobs, σ=σ, t=t, dist=Normal(0, σ))
+
+# Bounds on model parameters #################################################################
+λmin, λmax = (0.00, 0.05)
+Kmin, Kmax = (50., 150.)
+C0min, C0max = (0.0, 50.)
+
+θG = [λ, K, C0]
+lb = [λmin, Kmin, C0min]
+ub = [λmax, Kmax, C0max]
+
+##############################################################################################
+# Section 9: Find MLE by numerical optimisation, visually compare data and MLE solution
+# Use Nelder-Mead algorithm to estimate maximum likelihood solution for parameters given 
+# noisy data
+(xopt, fopt) = optimise(funmle, θG, lb, ub)
+fmle=fopt
+λmle, Kmle, C0mle = xopt .* 1.0
+θmle = [λmle, Kmle, C0mle]
+ymle(t) = Kmle*C0mle/((Kmle-C0mle)*exp(-λmle*t)+C0mle) # full solution
+
+# initialisation. model is a mutable struct that is currently intended to hold all model information
+likelihoodFunc = loglhood
+θnames = [:λ, :K, :C0]
+confLevel = 0.95
+model = initialiseLikelihoodModel(likelihoodFunc, data, θnames, θG, lb, ub)
+getMLE_ellipse_approximation!(model)
+
+ind1, ind2 = 2, 3
+
+newLb, newUb, initGuess, θranges, λranges = init_bivariate_parameters(model, ind1, ind2)
+
+pointa = generate_N_equally_spaced_points(1, model.ellipse_MLE_approx.Γmle, model.core.θmle, ind1, ind2, confidence_level=0.1, start_point_shift=0.0)
+
+uhat = [0.0, 0.0]
+
+p = (consistent=get_consistent_tuple(model, 0.95, LogLikelihood(), 2), initGuess=initGuess, newLb=newLb, newUb=newUb, λ_opt=zeros(1), θranges=θranges, λranges=λranges, ind1=ind1, ind2=ind2, uhat=uhat, pointa=pointa[:,1])
+# g(x) = bivariateΨ_gradient!(x, p)
+
+function bivariateΨ_gradient!(Ψ::Vector, p)
+    θs=zeros(eltype(Ψ), p.consistent.num_pars)
+
+    θs[p.ind1], θs[p.ind2] = Ψ
+    variablemapping2d!(θs, p.λ_opt, p.θranges, p.λranges)
+    return p.consistent.loglikefunction(θs, p.consistent.data)
+end
+
+# given a point, choose search direction
+function gradient_all(x); likelihoodFunc(x, data) end
+
+function gradient_2d(x); bivariateΨ_gradient!(x, p) end
+
+p.uhat .= [0.0,0.0]
+# once found a given point, find optimised nuisance parameters and store in p.λ_opt for use in the 2d (and also 1d) gradient calculation
+bivariateΨ_vectorsearch!(0.0, p) 
+
+fullgrad = ForwardDiff.gradient(gradient_all, [p.pointa..., p.λ_opt...])
+searchdir = -fullgrad[[ind1, ind2]] / norm(fullgrad[[ind1,ind2]],2)
+
+twodgrad = ForwardDiff.gradient(gradient_2d, p.pointa)
+searchdir = -twodgrad / norm(twodgrad,2)
+
+# p.uhat .= -twodgrad#searchdir
+p.uhat .= searchdir
+
+
+function mappedLikeFunction(Ψ, p)
+    θs=zeros(eltype(Ψ), p.consistent.num_pars)
+    Ψxy=zeros(eltype(Ψ), 2)
+    Ψxy .= p.pointa .+ Ψ[1]*p.uhat
+
+    θs[p.ind1], θs[p.ind2] = Ψxy
+    variablemapping2d!(θs, p.λ_opt, p.θranges, p.λranges)
+    return p.consistent.loglikefunction(θs, p.consistent.data)-p.consistent.targetll
+end
+
+bivariateΨ_vectorsearch!(0.00, p)
+p.pointa
+
+# g!(gvec, x)
+gvec=[0.0]
+function grad_Ψ_ForwardDiff!(gvec, x)
+    bivariateΨ_vectorsearch!(x[1], p)
+    gvec .= ForwardDiff.gradient(grad_Ψ!, x)
+    gvec 
+end
+
+function grad_Ψ_ForwardDiff_fj!(gvec, x)
+    gvec .= ForwardDiff.gradient(grad_Ψ!, x)
+    gvec 
+end
+
+# f(x)
+function grad_Ψ!(Ψ); mappedLikeFunction(Ψ, p) end
+
+grad_Ψ_ForwardDiff!(gvec, [0.0])
+
+mappedLikeFunction(0.00, p)
+
+ϵ=1e-8
+
+println("find_zero methods:")
+@btime psi = find_zero(bivariateΨ_vectorsearch!, 0.0, atol=ϵ, Roots.Order0(); p=p)
+@btime psi = find_zero(bivariateΨ_vectorsearch!, 0.0, atol=ϵ, Roots.Order8(); p=p)
+
+p.pointa .+ psi*p.uhat
+mappedLikeFunction(psi, p)
+
+
+using LineSearches
+using NLsolve
+
+println("nlsolve methods:")
+
+function NL_bivariateΨ_vectorsearch!(F, x)
+    F[1] = bivariateΨ_vectorsearch!(x[1], p)
+end 
+
+fp = nlsolve(NL_bivariateΨ_vectorsearch!, [0.0], method=:trust_region)
+p.pointa .+ fp.zero.*p.uhat
+
+
+c = zeros(1)
+NL_bivariateΨ_vectorsearch!(c, fp.zero)
+
+
+function myfun!(F, J, x)
+    if !(F == nothing) && !(J == nothing)
+        NL_bivariateΨ_vectorsearch!(F, x)
+        grad_Ψ_ForwardDiff_fj!(J, x)
+        return nothing
+    end
+    if !(F == nothing)
+        NL_bivariateΨ_vectorsearch!(F, x)
+        return nothing
+    end
+    if !(J == nothing)
+        grad_Ψ_ForwardDiff!(J, x)
+        return nothing
+    end
+end
+
+# may not be able to guarantee global convergence but should be ok in general 
+# doesn't work well when encounters singular matrices. Is fast otherwise
+# @btime nlsolve(NL_bivariateΨ_vectorsearch!, [0.0], method=:anderson);
+# fp = nlsolve(NL_bivariateΨ_vectorsearch!, [0.0], method=:anderson);
+# p.pointa .+ fp.zero.*p.uhat
+
+@btime nlsolve(NL_bivariateΨ_vectorsearch!, [0.0], method=:trust_region);
+
+@btime nlsolve(NL_bivariateΨ_vectorsearch!, grad_Ψ_ForwardDiff!, [0.0], method=:trust_region);
+@btime nlsolve(NL_bivariateΨ_vectorsearch!, grad_Ψ_ForwardDiff!, [0.0], method=:newton, linesearch=MoreThuente());
+
+# best versions - any are quite fast
+@btime nlsolve(only_fj!(myfun!), [0.0], method=:trust_region);
+@btime nlsolve(only_fj!(myfun!), [0.0], method=:newton, linesearch=MoreThuente());
+@btime nlsolve(only_fj!(myfun!), [0.0], method=:newton, linesearch=Static());
--- 2d_boundary_via_continuation.jl
+++ 2d_boundary_via_continuation.jl
@@ -4,18 +4,103 @@ using LinearAlgebra
 using Plots
 gr()
 
-e = construct_ellipse(10., 1.0)
-num_points=5
+include("JuLikelihood.jl")
+
+e = construct_ellipse(1., 1.0)
+num_points=15
+ind_lambda = 1
+ind1, ind2 = 2,3
+Random.seed!(12348)
+
+# Workflow functions ##########################################################################
+
+# Section 2: Define ODE model
+function DE!(dC, C, p, t)
+    λ,K=p
+    dC[1]= λ * C[1] * (1.0 - C[1]/K)
+end
+
+# Section 3: Solve ODE model
+function odesolver(t, λ, K, C0)
+    p=(λ,K)
+    tspan=(0.0, t[end])
+    prob=ODEProblem(DE!, [C0], tspan, p)
+    sol=solve(prob, saveat=t)
+    return sol[1,:]
+end
+
+# Section 4: Define function to solve ODE model 
+function ODEmodel(t, a)
+    y=odesolver(t, a[1],a[2],a[3])
+    return y
+end
+
+# Section 6: Define loglikelihood function
+function loglhood(a, data)
+    y=ODEmodel(data.t, a)
+    return sum(loglikelihood(data.dist, data.yobs-y))
+end
+
+
+# Section 8: Function to be optimised for MLE
+# note this function pulls in the globals, data and σ and would break if used outside of 
+# this file's scope
+function funmle(a); return loglhood(a, data) end 
+
+# Data setup #################################################################################
+# true parameters
+λ=0.01; K=100.0; C0=10.0; t=0:100:1000; σ=10.0;
+tt=0:5:1000
+a=[λ, K, C0]
+
+# true data
+ytrue = ODEmodel(t, a)
+
+# noisy data
+yobs = ytrue + σ*randn(length(t))
+
+# Named tuple of all data required within the likelihood function
+data = (yobs=yobs, σ=σ, t=t, dist=Normal(0, σ))
+
+# Bounds on model parameters #################################################################
+λmin, λmax = (0.00, 0.05)
+Kmin, Kmax = (50., 150.)
+C0min, C0max = (0.0, 50.)
+
+θG = [λ, K, C0]
+lb = [λmin, Kmin, C0min]
+ub = [λmax, Kmax, C0max]
+θnames = ["λ", "K", "C0"]
+
+##############################################################################################
+# Section 9: Find MLE by numerical optimisation, visually compare data and MLE solution
+# Use Nelder-Mead algorithm to estimate maximum likelihood solution for parameters given 
+# noisy data
+(xopt, fopt) = optimise(funmle, θG, lb, ub)
+fmle=fopt
+λmle, Kmle, C0mle = xopt .* 1.0
+θmle = [λmle, Kmle, C0mle]
+ymle(t) = Kmle*C0mle/((Kmle-C0mle)*exp(-λmle*t)+C0mle) # full solution
+
+
+##############################################################################################
+# Section 12: 
+# 3D approximation of the likelihood around the MLE solution
+H, Γ = getMLE_hessian_and_covariance(funmle, θmle)
+
+##############################################################################################
+##############################################################################################
+##############################################################################################
 
 # place N equally spaced points on boundary of ellipse approximation at some small confidence level.
-# generateN_equally_spaced_points(10, Γ, θmle, ind1, ind2)
-points = generate_N_equally_spaced_points(num_points, e, start_point_shift=0.0)
+# points = generate_N_equally_spaced_points(num_points, e, start_point_shift=0.0)
+points = generate_N_equally_spaced_points(num_points, Γ, θmle, ind1, ind2, 
+                confidence_level=0.1, start_point_shift=0.0)
 
 # determine true loglikelihood function values at all N points
 # ll_vals = .....
 ll_vals = zeros(num_points) .- 0.03
 # θmle = zeros(model.core.num_pars)
-θmle = zeros(3)
 
 gradient_i = zeros(num_points)
 # for i in 1:num_points
@@ -26,6 +111,40 @@ gradient_i = zeros(num_points)
 
 # calculate normal at each point
 # Also implement via forward diff - gives both magnitude and 
+f(x) = analytic_ellipse_loglike(x, [ind1, ind2], (θmle=θmle, Γmle=Γ))
+ForwardDiff.gradient(f, [1.0,0.0])
+
+
+function f_full(x::Vector) 
+    θs=zeros(eltype(x), 3)
+    θs[ind1], θs[ind2] = x[1:2]
+    θs[ind_lambda] = x[3]
+    ellipse_loglike(θs[:], (θmle=θmle, Hmle=H))
+end
+
+function bivariateΨ_ellipse!(x)
+    θs=zeros(3)
+    
+    function fun(λ)
+        θs[ind1], θs[ind2] = x
+        θs[ind_lambda] = λ[1]
+        return ellipse_loglike(θs, (θmle=θmle, Hmle=H))
+    end
+    (xopt,fopt)=optimise(fun, [θmle[ind_lambda]], [lb[ind_lambda]], [ub[ind_lambda]])
+    # return fun(0.01)
+    return xopt[1]
+end
+
+ellipse_loglike(θmle, (θmle=θmle, Hmle=H))
+
+f([100, 10])
+lambda=0.01
+f_full([100.0, 10.0, lambda])
+
+bivariateΨ_ellipse!([100, 10])
+
+ForwardDiff.gradient(f, [1.0,0.0])
+
 normal_vectors = zeros(2, num_points)
 
 # NOTE: METHOD REQUIRES THERE TO BE AT LEAST 3 POINTS
@@ -42,5 +161,24 @@ for i in 2:num_points-1
    normal_vector!(normal_vectors, i, @view(points[:, i-1]), @view(points[:, i+1]))
 end
 
-boundary = scatter(points[1,:], points[2,:], aspect_ratio = :equal)
-quiver!(points[1,:], points[2,:], quiver=(normal_vectors[1,:], normal_vectors[2,:]))
+boundary = scatter(points[1,:], points[2,:], label="Ellipse", markersize=6)
+xlabel!(θnames[ind1])
+ylabel!(θnames[ind2])
+# quiver!(points[1,:], points[2,:], lw=2, quiver=(normal_vectors[1,:], normal_vectors[2,:]))
+
+normal_vectors_true = zeros(2, num_points)
+for i in 1:num_points
+    normal_vectors_true[:,i] .= -ForwardDiff.gradient(f, points[:,i])
+    # normal_vectors_true[:, i] .= @view(normal_vectors_true[:, i]) / norm(@view(normal_vectors_true[:,i])) 
+end
+
+quiver!(points[1,:], points[2,:], lw=4, linecolor=:maroon, quiver=(normal_vectors_true[1,:], normal_vectors_true[2,:]))
+
+normal_vectors_full = zeros(2, num_points)
+for i in 1:num_points
+    global lambda = bivariateΨ_ellipse!(points[:,i])
+    normal_vectors_full[:,i] .= -ForwardDiff.gradient(f_full, [points[:,i]..., lambda])[1:2]
+    # normal_vectors_full[:, i] .= @view(normal_vectors_full[:, i]) / norm(@view(normal_vectors_full[:,i])) 
+end
+
+quiver!(points[1,:], points[2,:], lw=1, linecolor=:lightgrey, quiver=(normal_vectors_full[1,:], normal_vectors_full[2,:]))
