46c30e8f06842610bc2fcd31e15763764ebb6eb5
JoelTrent <79883375+JoelTrent@users.noreply.github.com>
2023-06-23 11:23:59 +1200

    Add bivariate parameter coverage. Rename coverage to uni parameter coverage

--- Workflow paper examples/logistic_packagetest_uni_par_coverage.jl
+++ Workflow paper examples/logistic_packagetest_uni_par_coverage.jl
@@ -3,33 +3,12 @@
 
 using Distributed
 using PlaceholderLikelihood
-addprocs(10)
+# addprocs(10)
 # @everywhere using Revise
 @everywhere using DifferentialEquations, Random, Distributions
 @everywhere using PlaceholderLikelihood
 
 # Workflow functions ##########################################################################
-
-# Section 2: Define ODE model
-# @everywhere function DE!(dC, C, p, t)
-#     λ,K=p
-#     dC[1]= λ * C[1] * (1.0 - C[1]/K)
-# end
-
-# # Section 3: Solve ODE model
-# @everywhere function odesolver(t, λ, K, C0)
-#     p=(λ,K)
-#     tspan=(0.0, t[end])
-#     prob=ODEProblem(DE!, [C0], tspan, p)
-#     sol=solve(prob, saveat=t, verbose=false)
-#     return sol[1,:]
-# end
-
-# # Section 4: Define function to solve ODE model 
-# @everywhere function ODEmodel(t, a)
-#     y=odesolver(t, a[1],a[2],a[3])
-#     return y
-# end
 
 @everywhere function solvedmodel(t, a)
     return (a[2]*a[3]) ./ ((a[2]-a[3]) .* (exp.(-a[1] .* t)) .+ a[3])
@@ -78,24 +57,10 @@ C0min, C0max = (0.0, 50.)
 lb = [λmin, Kmin, C0min]
 ub = [λmax, Kmax, C0max]
 par_magnitudes = [0.005, 10, 10]
-# par_magnitudes = [1, 1, 1]
-
-##############################################################################################
-# Section 9: Find MLE by numerical optimisation, visually compare data and MLE solution
-# Use Nelder-Mead algorithm to estimate maximum likelihood solution for parameters given 
-# noisy data
-(xopt, fopt) = optimise(funmle, θG, lb, ub)
-fmle=fopt
-λmle, Kmle, C0mle = xopt .* 1.0
-θmle = [λmle, Kmle, C0mle]
-ymle(t) = Kmle*C0mle/((Kmle-C0mle)*exp(-λmle*t)+C0mle) # full solution
-
 
 @everywhere likelihoodFunc = loglhood
 θnames = [:λ, :K, :C0]
-confLevel = 0.95
 
-# initialisation. model is a mutable struct that is currently intended to hold all model information
 model = initialiseLikelihoodModel(likelihoodFunc, data, θnames, θG, lb, ub, par_magnitudes);
 
 @everywhere function data_generator(θtrue, generator_args::NamedTuple)
